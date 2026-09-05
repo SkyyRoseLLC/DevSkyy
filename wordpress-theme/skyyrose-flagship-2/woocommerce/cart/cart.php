0bcc540747ea3e4df104dd0efa8f92008e7a59c7
DevSkyy Agent <skyyroseco@gmail.com>
2026-09-05 07:44:26 -0700

    fix(cart): preserve native quantity controls

--- wordpress-theme/skyyrose-flagship-2/woocommerce/cart/cart.php
+++ wordpress-theme/skyyrose-flagship-2/woocommerce/cart/cart.php
@@ -68,18 +68,16 @@ else :
 					</div>
 					<div class="sr2-cart__controls">
 						<?php
-						echo wp_kses_post(
-							woocommerce_quantity_input(
-								array(
-									'input_name'   => "cart[{$cart_item_key}][qty]",
-									'input_value'  => $cart_item['quantity'],
-									'max_value'    => $product->get_max_purchase_quantity(),
-									'min_value'    => '0',
-									'product_name' => $product_name,
-								),
-								$product,
-								false
+						// WooCommerce escapes its native control; post-content KSES removes inputs.
+						woocommerce_quantity_input(
+							array(
+								'input_name'  => "cart[{$cart_item_key}][qty]",
+								'input_value' => $cart_item['quantity'],
+								'max_value'   => $product->get_max_purchase_quantity(),
+								'min_value'   => '0',
+								'product_name' => $product_name,
 							),
+							$product
 						);
 						?>
 						<?php
